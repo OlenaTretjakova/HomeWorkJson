cbde01b57711da3a7c8fdd19f70f4bd90daa5d48
Olena Tr <elenaprim70@gmail.com>
2024-05-14 18:43:36 +0300

    Add project files.

---
+++ Fraction.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HomeWorkJson
+{
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}, ";
+        }
+
+        public static string SerializeJson(string file, Fraction fraction)
+        {
+            List<Fraction> list = new List<Fraction>();
+
+            if (File.Exists(file))
+            {
+                string existJsonFile = File.ReadAllText(file);
+
+                list = JsonConvert.DeserializeObject<List<Fraction>>(existJsonFile);
+
+                list.Add(fraction);
+
+                string serializeContent = JsonConvert.SerializeObject(list);
+
+                return serializeContent;
+            }
+            else
+            {
+                list.Add(fraction);
+                return JsonConvert.SerializeObject(list);
+            }
+        }
+
+        public static void WrieteToFile(string file,string json)
+        { 
+            File.WriteAllText(file, json);
+        }
+
+        public static string ReadFile(string file)
+        {
+            return File.ReadAllText(file);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fr1 = new Fraction(1, 2);
+            Fraction fr2 = new Fraction(3, 2);
+            Fraction fr3 = new Fraction(1, 10);
+
+            Console.WriteLine(fr1.ToString());
+            Console.WriteLine(fr2.ToString());
+            Console.WriteLine(fr3.ToString());
+
+            Console.WriteLine();
+
+            string json = Fraction.SerializeJson("fraction.txt", fr1);
+            Fraction.WrieteToFile("fraction.txt", json);
+
+            string json1 = Fraction.SerializeJson("fraction.txt", fr2);
+            Fraction.WrieteToFile("fraction.txt", json1);
+
+            string json2 = Fraction.SerializeJson("fraction.txt", fr3);
+            Fraction.WrieteToFile("fraction.txt", json2);
+
+            string strFileJson = Fraction.ReadFile("fraction.txt");
+            List<Fraction> list = JsonConvert.DeserializeObject<List<Fraction>>(strFileJson);
+
+            foreach (var item in list)
+            {
+                Console.Write(item.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
